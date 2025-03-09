506983f43b2944a66f8bc8438925c87ae6f72596
Железнякович Максим <m.zheleznyakovich@bft.ru>
2025-03-09 17:12:23 +0300

    Initial commit

---
+++ src/main/java/com/example/resmanback/ResmanBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.resmanback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResmanBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResmanBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/resmanback/ResmanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resmanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResmanBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
